15b4986172723bf301d1a8677753a324cd6b3efc
Pavel Kholopov <ironface13@gmail.com>
2024-03-29 11:27:36 +0300

    deploy

--- src/components/Calendar/CalendarHeader.vue
+++ src/components/Calendar/CalendarHeader.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script setup>
+
+//https://disk.yandex.ru/i/BHBs2kKeS9ptXg
 import { localization } from '@/utils/localization'
 import { useLocaleStore } from '@/stores/locale.js'
 import { storeToRefs } from 'pinia';
